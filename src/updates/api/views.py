be7ee2837b679680db3a485de3abec1fe57aa262
CFE <hello@teamcfe.com>
2017-12-07 10:52:08 -0800

    10 - Pratical API Module

---
+++ src/updates/api/views.py
@@ -0,0 +1,30 @@
+
+
+from updates.models import Update as UpdateModel
+
+
+
+# Creating, Updating, Deleting, Retrieving (1) -- Update Model
+
+class UpdateModelDetailAPIView():
+    def get(self, request, *args, **kwargs):
+
+        return #json
+
+    def post(self, request, *args, **kwargs):
+
+        return #json 
+
+    def put(self, request, *args, **kwargs):
+
+        return #json 
+
+    def delete(self, request, *args, **kwargs):
+
+        return #json 
+
+
+class UpdateModelListAPIView():
+    def get(self, request, *args, **kwargs):
+
+        return #json
