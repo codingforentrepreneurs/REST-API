bdf02df3d5b6db837afec1ca79492e28d0728579
CFE <hello@teamcfe.com>
2017-12-06 18:10:20 -0800

    8 - Managers & Methods to Produce Serialized Data

--- src/updates/models.py
+++ src/updates/models.py
@@ -1,8 +1,40 @@
+import json
+
+from django.core.serializers import serialize
 from django.conf import settings
 from django.db import models
 
 def upload_update_image(instance, filename):
     return "updates/{user}/{filename}".format(user=instance.user, filename=filename)
+
+
+
+class UpdateQuerySet(models.QuerySet):
+    # def serialize(self):
+    #     qs = self 
+    #     return serialize('json', qs, fields=('user', 'content', 'image'))
+
+    # def serialize(self):
+    #     qs = self 
+    #     final_array = []
+    #     for obj in qs:
+    #         stuct = json.loads(obj.serialize())
+    #         final_array.append(stuct)
+    #     return json.dumps(final_array)
+
+    def serialize(self):
+        qs = self 
+        final_array = []
+        for obj in qs:
+            stuct = json.loads(obj.serialize())
+            final_array.append(stuct)
+        return json.dumps(final_array)
+
+
+class UpdateManager(models.Manager):
+    def get_queryset(self):
+        return UpdateQuerySet(self.model, using=self._db)
+
 
 class Update(models.Model):
     user            = models.ForeignKey(settings.AUTH_USER_MODEL)
@@ -11,5 +43,16 @@ class Update(models.Model):
     updated         = models.DateTimeField(auto_now=True)
     timestamp       = models.DateTimeField(auto_now_add=True)
 
+    objects = UpdateManager()
+
     def __str__(self):
         return self.content or ""
+
+    def serialize(self):
+        json_data = serialize("json", [self], fields=('user', 'content', 'image'))
+        stuct = json.loads(json_data) # [{}]
+        print(stuct)
+        data = json.dumps(stuct[0]['fields'])
+        return data
+
+
--- src/updates/views.py
+++ src/updates/views.py
@@ -51,29 +51,14 @@ class JsonCBV2(JsonResponseMixin, View):
 class SerializedDetialView(View):
      def get(self, request, *args, **kwargs):
         obj = Update.objects.get(id=1)
-        data = serialize("json", [obj,], fields=('user', 'content'))
-        #print(data)
-        json_data = data
-        # data = {
-        #     "user": obj.user.username,
-        #     "content": obj.content
-        # }
-        # json_data = json.dumps(data)
+        json_data = obj.serialize()
         return HttpResponse(json_data, content_type='application/json')
 
 
 class SerializedListView(View):
      def get(self, request, *args, **kwargs):
         qs = Update.objects.all()
-        data = serialize("json", qs, fields=('user', 'content'))
-        print(data)
-        json_data = data
-        # data = {
-        #     "user": obj.user.username,
-        #     "content": obj.content
-        # }
-        #json_data = json.dumps(data)
-
+        json_data = Update.objects.all().serialize()
         return HttpResponse(json_data, content_type='application/json')
 
 
