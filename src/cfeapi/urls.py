76d5107713b892059f0ed025a74c6d4f834d672f
CFE <hello@teamcfe.com>
2017-12-06 17:18:13 -0800

    4 - A JSON Response

--- src/cfeapi/urls.py
+++ src/cfeapi/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 
+from updates.views import update_model_detail_view
+
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^$', update_model_detail_view),
 ]
--- src/updates/views.py
+++ src/updates/views.py
@@ -1,3 +1,19 @@
+from django.http import JsonResponse, HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+
+from .models import Update
+#def detial_view(request):
+    #return render(request, template, {}) # return JSON data  --> JS Object Notion
+    #return HttpResponse(get_template().render({}))
+
+
+def update_model_detail_view(request):
+    '''
+    URI -- for a REST API
+    '''
+    data = {
+        "count": 1000,
+        "content": "Some new content"
+    }
+    return JsonResponse(data)
