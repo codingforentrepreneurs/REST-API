5083b7665b5e5166cd37177bbaef8db41a1a4096
CFE <hello@teamcfe.com>
2017-12-06 18:01:14 -0800

    7 - Serialize Data

--- src/cfeapi/urls.py
+++ src/cfeapi/urls.py
@@ -16,11 +16,20 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 
-from updates.views import json_example_view, JsonCBV, JsonCBV2
+from updates.views import (
+            json_example_view, 
+            JsonCBV, 
+            JsonCBV2, 
+            SerializedDetialView, 
+            SerializedListView
+    )
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^json/cbv/$', JsonCBV.as_view()),
+
     url(r'^json/cbv2/$', JsonCBV2.as_view()),
     url(r'^json/example/$', json_example_view),
+    url(r'^json/serialized/detail/$', SerializedDetialView.as_view()),
+    url(r'^json/serialized/list/$', SerializedListView.as_view()),
 ]
--- src/updates/views.py
+++ src/updates/views.py
@@ -1,6 +1,6 @@
 import json
 
-
+from django.core.serializers import serialize
 from django.http import JsonResponse, HttpResponse
 from django.shortcuts import render
 from django.views.generic import View
@@ -45,6 +45,39 @@ class JsonCBV2(JsonResponseMixin, View):
             "content": "Some new content"
         }
         return self.render_to_json_response(data)
+
+
+
+class SerializedDetialView(View):
+     def get(self, request, *args, **kwargs):
+        obj = Update.objects.get(id=1)
+        data = serialize("json", [obj,], fields=('user', 'content'))
+        #print(data)
+        json_data = data
+        # data = {
+        #     "user": obj.user.username,
+        #     "content": obj.content
+        # }
+        # json_data = json.dumps(data)
+        return HttpResponse(json_data, content_type='application/json')
+
+
+class SerializedListView(View):
+     def get(self, request, *args, **kwargs):
+        qs = Update.objects.all()
+        data = serialize("json", qs, fields=('user', 'content'))
+        print(data)
+        json_data = data
+        # data = {
+        #     "user": obj.user.username,
+        #     "content": obj.content
+        # }
+        #json_data = json.dumps(data)
+
+        return HttpResponse(json_data, content_type='application/json')
+
+
+
 
 
 
